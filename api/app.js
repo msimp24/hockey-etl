d95667e4d2f7ff28b307bb2e59d4cda082a0da5d
Mark Simpson <mark.simpson4@gmail.com>
2025-11-05 11:37:32 -0400

    updates to welcome

--- api/app.js
+++ api/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
 const app = express()
 
 app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 const goalieRoutes = require('./routes/goalieRoutes')
@@ -17,8 +19,18 @@ app.use('/standings', standingsRoutes)
 app.use('/matchups', matchupRoutes)
 app.use('/email', emailRoutes)
 
+const filePath = path.join(__dirname, 'views')
+
 app.get('/', (req, res) => {
-  res.send('Welcome to my hockey stats API')
+  res.sendFile(filePath + '/welcome.html', (err) => {
+    console.log('Error loading HTML file')
+  })
+})
+
+app.get('/email', (req, res) => {
+  res.sendFile(filePath + '/email-sub.html', (err) => {
+    console.log('Error loading HTML file')
+  })
 })
 
 module.exports = app
